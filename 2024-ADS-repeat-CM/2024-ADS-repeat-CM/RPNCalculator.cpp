9994a6f9e5b46da57fee7749ffbef2de389f235a
CiaranM78 <ciaran7878@gmail.com>
2024-05-21 21:21:51 +0100

    log print start (wip)
    wrote the foundation code to print out the calculators inputs into (needs to be linked to stack along with the main cpp file

--- 2024-ADS-repeat-CM/2024-ADS-repeat-CM/RPNCalculator.cpp
+++ 2024-ADS-repeat-CM/2024-ADS-repeat-CM/RPNCalculator.cpp
@@ -7,22 +7,27 @@
 using namespace std;
 
 void welcomemessage() {
-	//Enter c to clear the stack
-	//	s to square
-	//	n to negate
-	//	p to pop current value
-	//	q to quit
-	//	X > 46
-	//	46 > 10
-	//	10 > +
-	//	56 > c
-	//	X > hello
-	//	invalid input
-	//	X >
-
+	
+cout << "Hi my name is Ciaran Murtagh and this is my Calculator that uses Reverse Polish Notation or RPN for short." << endl;
+ cout << "Enter c to clear the stack." << endl;
+ cout << "t to add." << endl;
+ cout << "y to subtract." << endl;
+ cout << "u to divide." << endl;
+ cout << "i to multiply." << endl;
+ cout << "s to square." << endl;
+ cout << "n to negate." << endl;
+ cout << "p to pop current value." << endl;
+ cout << "q to quit." << endl;
 }
 
 void CalculatorOperation() {
+
+	 
+	int currentinput;
+
+	cout << "enter calculater value" << endl;
+	cin >> currentinput;
+
 
 
 }
---
+++ 2024-ADS-repeat-CM/2024-ADS-repeat-CM/RPNLogPrint.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "RPNCalculator.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "RPNCalculator.cpp"
+
+using namespace std;
+
+void writelog() {
+
+	RPNCalculator<int> history;
+	ifstream RPNhistory;
+	RPNhistory.open("C:\2024-- - ADS-- - Repeat-- - CM\2024 - ADS - repeat - CM\RPNCalculatorTest\RPN.log.csv");
+
+	RPNhistory.open("RPN.log.csv", ios::out);
+
+
+	string historyline;
+
+	while (getline(RPNhistory, historyline)) {
+		string outputnumber;
+
+		stringstream RPNlog(historyline);
+		
+
+	}
+
+	RPNhistory.close();
+
+
+}
